f3da28158578b97a4b1ad0a207e65df1feea642e
JohnDRyanIV <43046528+JohnDRyanIV@users.noreply.github.com>
2022-04-29 11:36:21 -0500

    updated dbinfo class
    removed references to my own database from DBInfo class

--- src/database/DBInfo.java
+++ src/database/DBInfo.java
@@ -12,9 +12,9 @@ import javax.swing.JOptionPane;
  *
  */
 public class DBInfo {
-	private final String url = "jdbc:mysql://localhost:3306/DSFinal"; // DATABASE URL
-	private final String user = "root";
-	private final String password = "chocolate430";
+	private final String url = "Your Database String"; // DATABASE URL
+	private final String user = "Your Username";
+	private final String password = "Your Password";
 	
 	/**
 	 * @return - url of database
@@ -45,7 +45,6 @@ public class DBInfo {
 		try {
 			return DriverManager.getConnection(url, user, password);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			printErrorMessage("The database cannot be accessed. Stack trace has been printed to console.\n " +
 					"If you are seeing this message, please notify your system administrator.");
